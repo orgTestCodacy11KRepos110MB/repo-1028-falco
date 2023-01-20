0401cddc3bb479275c41025d0f83ba7df4c46bd6
Jason Dellaluce <jasondellaluce@gmail.com>
2023-01-20 19:34:26 +0000

    new(regression-tests): add initial sample tests
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

---
+++ regression-tests/tests/cli_test.go
@@ -0,0 +1,49 @@
+package tests
+
+import (
+	"os/user"
+	"regexp"
+	"testing"
+
+	"github.com/falcosecurity/falco/regression-tests/pkg/falco/run"
+	"github.com/falcosecurity/falco/regression-tests/pkg/falco/test"
+)
+
+var DefaultFalcoBinaryPath = "/usr/bin/falco"
+var IsRoot = false
+
+func init() {
+	user, err := user.Current()
+	if err != nil {
+		panic("could not get the current user")
+	}
+	IsRoot = user.Username == "root"
+}
+
+func TestBadConfigNoOutput(t *testing.T) {
+	test.TestProcess(t,
+		run.NewProcRunner(DefaultFalcoBinaryPath),
+		test.ExitCode(1),
+		test.StderrMatch(regexp.MustCompile(".*No outputs configured.*")),
+	)
+}
+
+func TestArgVersion(t *testing.T) {
+	c := run.Config{}
+	c.StdoutOutput.Enabled = true
+	test.TestProcess(t,
+		run.NewProcRunner(DefaultFalcoBinaryPath),
+		test.Config(&c),
+		test.Args("--version"),
+		test.StdoutMatch(regexp.MustCompile(
+			"Falco version:[\\s]+[0-9]+\\.[0-9]+\\.[0-9][\\s]+"+
+				"Libs version:[\\s]+[0-9]+\\.[0-9]+\\.[0-9][\\s]+"+
+				"Plugin API:[\\s]+[0-9]+\\.[0-9]+\\.[0-9][\\s]+"+
+				"Driver:[\\s]+"+
+				"API version:[\\s]+[0-9]+\\.[0-9]+\\.[0-9][\\s]+"+
+				"Schema version:[\\s]+[0-9]+\\.[0-9]+\\.[0-9][\\s]+"+
+				"Default driver:[\\s]+[0-9]+\\.[0-9]+\\.[0-9]\\+driver")),
+		test.ExitCode(0),
+	)
+	// todo: test json output too
+}
